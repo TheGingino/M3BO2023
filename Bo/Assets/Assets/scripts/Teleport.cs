370fc72652448dcc09a07c73fc8e9a14cec8bd6a
Gino <35615@ma-web.nl>
2023-04-03 11:15:46 +0200

    Teleport functie af

---
+++ Bo/Assets/Assets/scripts/Teleport.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Transform Target;
+    public GameObject Player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player.transform.position = Target.transform.position;
+    }
+
+
+}
